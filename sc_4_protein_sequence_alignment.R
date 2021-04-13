f003eb1518969989eb6dfcdcfde5def17a09748e
Greerlab <74384499+Greerlab@users.noreply.github.com>
2021-04-13 10:47:23 -0400

    Update sc_4_protein_sequence_alignment.R

--- sc_4_protein_sequence_alignment.R
+++ sc_4_protein_sequence_alignment.R
@@ -1,12 +1,10 @@
 library(Biostrings)
 library(protr)
 
-setwd("~/Desktop/phylo/")
-
-mySequences <- readAAStringSet("dedup4.OR.fa", format = "fasta")
+mySequences <- readAAStringSet("data/dedup4.OR.fa", format = "fasta") #OR protein sequence downloaded from NCBI
 mySequences = removeGaps(mySequences,pattern = " ")
 d2 = parSeqSim(mySequences, cores = 4, type = "global")
 d2[is.na(d2)]=0
 rownames(d2) = names(mySequences)
 colnames(d2) = names(mySequences)
-saveRDS(d2,"OR_similarity_score_defult.rds")
+saveRDS(d2,"OR_similarity_score_default.rds")
